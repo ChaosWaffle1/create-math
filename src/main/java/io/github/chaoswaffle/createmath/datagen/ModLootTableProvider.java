c6a1a591358cd107e36c562c101d64ed210be433
Jack Curtis <31493133+ChaosWaffle1@users.noreply.github.com>
2024-07-04 14:21:09 -0400

    add basic loot table for axiomite ore

---
+++ src/main/java/io/github/chaoswaffle/createmath/datagen/ModLootTableProvider.java
@@ -0,0 +1,15 @@
+package io.github.chaoswaffle.createmath.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider;
+
+public class ModLootTableProvider extends FabricBlockLootTableProvider {
+    protected ModLootTableProvider(FabricDataOutput dataOutput) {
+        super(dataOutput);
+    }
+
+    @Override
+    public void generate() {
+
+    }
+}
